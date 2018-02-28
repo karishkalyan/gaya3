9337a6c14da3e4c30449adfee780ed89cba1c829
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-28 15:52:30 +0530

    Create prgm68.c

---
+++ prgm68.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\n enter the num:");
+scanf("%d",&a);
+if(a%7==0)
+printf"\ it is a multiple of 7");
+else
+printf("\n it is not a multiple of 7");
+getch();
+}
