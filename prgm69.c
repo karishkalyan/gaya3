073d05bf52dc389fc606d27d4bf45580e1ab2ee5
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-28 15:58:30 +0530

    Create prgm69.c

---
+++ prgm69.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,d;
+printf("\ enter two numbers:");
+scanf("%d%d",&n1,&n2);
+d=n1-n2;
+if(d%2==0)
+printf("\n it is  even");
+else
+printf("\n it is odd");
+getch();
+}
