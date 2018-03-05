cf7fd1f9559171772bb6f102ea2245deb0bbf38e
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-03-05 16:09:42 +0530

    Create prgm93.c

---
+++ prgm93.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int cel,kel;
+	printf("\n enter the temperature in celsius:");
+	scanf("%d",&cel);
+	kel=cel+273;
+	printf("\n equivalent kelvin value is : %d",kel);
+	getch();
+  }
