93b92a73e27debdbda455ea9df93cceac6bbcff7
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-03-09 15:45:30 +0530

    Create prgm102.c

---
+++ prgm102.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+int a;
+printf("\n Enter the number:");
+scanf("%d",&a);
+if(a%2==0)
+{
+while(a%2==0)
+{
+printf("\n%d",(a/2));
+a=a/2;
+}
+}
+else if(a%2!=0)
+{
+	printf("\n %d",a);
+}
+
+	getch();
+}
