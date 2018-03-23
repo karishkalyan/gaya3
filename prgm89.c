bf3408875e03cca50c8f77dee734517cd00819f9
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-03-23 16:04:31 +0530

    Create prgm89.c

---
+++ prgm89.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char a[10],t;
+  int l;
+  printf("Enter the string");
+  scanf("%s",a);
+  l=strlen(a);
+  for(int i=0;i<=l-1;i++)
+  {
+    for(int j=i+1;j<l;j++)
+    {
+      if(a[i]>a[j])
+      {
+        t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+      }
+    }
+  }
+  for(int i=0;i<=l;i++)
+  {
+    printf("%c",a[i]);
+  }
+ getch();
+}
